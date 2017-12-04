67cbd7e415e4174c3e6c84555cd3b5d7e10fd271
Thomas Ari Bech <thomasb17@ru.is>
2017-12-04 13:15:19 +0000

    Add base repo functions

--- Repositories/EmployeeRepository.cpp
+++ Repositories/EmployeeRepository.cpp
@@ -1,12 +1,38 @@
 #include "EmployeeRepository.h"
+#include <fstream>
 
-
-
-EmployeeRepository::EmployeeRepository()
-{
+EmployeeRepository::EmployeeRepository() {
+	this->file = "EmployeeRecords.bat";
 }
 
 
-EmployeeRepository::~EmployeeRepository()
-{
+EmployeeRepository::~EmployeeRepository() {}
+
+void EmployeeRepository::addRecord(Employee record) {
+	ofstream fout;
+	fout.open(this->file, ios::binary | ios::app);
+	int size  = record.name.length() + 1;
+	fout.write((char*)(&size), sizeof(int));
+	fout.write(record.name.c_str(), size);
+	fout.write(&record.ssn, sizeof(record.ssn));
+	fout.write((char*)(&record.salary), sizeof(int));
+	fout.write((char*)(&record.month), sizeof(int));
+	fout.write((char*)(&record.year), sizeof(int));
+	fout.close();
+}
+
+vector<Employee> EmployeeRepository::getRecords() {
+	ifstream fin;
+	vector<Employee> vec;
+	fin.open(this->file, ios::binary);
+	if (fin.is_open()) {
+		while (pos != endpos) {
+			Employee record;
+			int size, salary, ;
+			string name;
+
+		}
+		fin.close();
+	}
+	return vec;
 }
--- Repositories/EmployeeRepository.h
+++ Repositories/EmployeeRepository.h
@@ -1,8 +1,17 @@
 #pragma once
+
+#include <vector>
+#include "../Models/Employee.h"
+
 class EmployeeRepository
 {
+private:
+	string file;
 public:
 	EmployeeRepository();
 	~EmployeeRepository();
+	void addRecord(Employee record);
+	vector<Employee> getRecords();
+
 };
 
