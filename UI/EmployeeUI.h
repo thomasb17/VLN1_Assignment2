b9f30676f4525ef0f0d8c15247445615731e5000
Hugrún Hannesdóttir <hugrun17@ru.is>
2017-12-04 13:08:40 +0000

    EmployeeUI additions

--- UI/EmployeeUI.h
+++ UI/EmployeeUI.h
@@ -1,10 +1,14 @@
 #pragma once
 #include <iostream>
 using namespace std;
+
 class EmployeeUI
 {
+private: 
+	char input;
 public:
 	EmployeeUI();
 	void mainMenu();
+	void validateInput(char input);
 };
 
