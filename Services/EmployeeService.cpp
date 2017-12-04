3d1751b486576f56894322192a88960fa6558c7c
Thomas Ari Bech <thomasb17@ru.is>
2017-12-04 14:01:05 +0000

    Add base for EmployeeService

--- Services/EmployeeService.cpp
+++ Services/EmployeeService.cpp
@@ -6,3 +6,24 @@ EmployeeService::EmployeeService()
 {
 }
 
+void EmployeeService::addRecord(Employee record) {
+
+}
+
+vector<Employee> EmployeeService::getRecordsForSSN(char* ssn) {
+	vector<Employee> vec;
+
+	return vec;
+}
+
+int EmployeeService::getTotalSalary(int year, char* ssn) {
+	int total;
+
+	return total;
+}
+
+string EmployeeService::getHighestSalaryName(int year) {
+	string str;
+
+	return str;
+}
--- Services/EmployeeService.h
+++ Services/EmployeeService.h
@@ -1,7 +1,15 @@
 #pragma once
+
+#include "../Repositories/EmployeeRepository.h"
+
 class EmployeeService
 {
+private:
+	EmployeeRepository repo;
 public:
 	EmployeeService();
+	void addRecord(Employee record);
+	vector<Employee> getRecordsForSSN(char* ssn);
+	int getTotalSalary(int year, char* ssn);
+	string getHighestSalaryName(int year);
 };
-
